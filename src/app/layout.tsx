2c654a462505c8cb516ebe1ef1193e964690e41d
AlvaroLuken <al5031@nyu.edu>
2026-01-28 14:05:36 -0800

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default async function RootLayout({
   );
 
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <Providers initialState={initialState}>{children}</Providers>
       </body>
